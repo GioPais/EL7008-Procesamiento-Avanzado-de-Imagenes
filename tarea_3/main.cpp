78423093ded6ded30503a101726d3174e51f43bc
GioPais <gio.pais@hotmail.com>
2018-11-19 19:36:18 -0300

    Tunning

--- tarea_3/main.cpp
+++ tarea_3/main.cpp
@@ -51,80 +51,6 @@ Mat LBP(Mat img)
 	return output;
 }
 
-Mat LBP2(Mat img)
-{
-	Mat output = Mat::zeros(img.rows - 2, img.cols - 2, CV_8UC1);
-	int toULBP[256] = {
-		1,2,3,4,5,0,6,7,8,0,0,0,9,0,10,11,12,0,0,0,0,0,0,0,13,0,0,0,14,0,
-		15,16,17,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,18,0,0,0,0,0,0,0,19,
-		0,0,0,20,0,21,22,23,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-		0,0,0,0,0,0,0,0,0,0,0,0,24,0,0,0,0,0,0,0,0,0,0,0,0,0,
-		0,0,25,0,0,0,0,0,0,0,26,0,0,0,27,0,28,29,30,31,0,32,0,0,0,33,0,
-		0,0,0,0,0,0,34,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,35,0,0,0,0,
-		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-		0,36,37,38,0,39,0,0,0,40,0,0,0,0,0,0,0,41,0,0,0,0,0,0,0,0,0,
-		0,0,0,0,0,0,42,43,44,0,45,0,0,0,46,0,0,0,0,0,0,0,47,48,49,0,50,
-		0,0,0,51,52,53,0,54,55,56,57,58 };
-	for (int r = 0; r < img.rows - 2; r++) {
-		for (int c = 0; c < img.cols - 2; c++) {
-			// Current image window
-			Mat window = img(Rect(c, r, 3, 3));
-			// Clockwise binary codification
-			vector<int> binary_cod;
-			uchar threshold = window.at<uchar>(1, 1);
-			binary_cod.push_back(window.at<uchar>(0, 0) >= threshold);
-			binary_cod.push_back(window.at<uchar>(0, 1) >= threshold);
-			binary_cod.push_back(window.at<uchar>(0, 2) >= threshold);
-			binary_cod.push_back(window.at<uchar>(1, 2) >= threshold);
-			binary_cod.push_back(window.at<uchar>(2, 2) >= threshold);
-			binary_cod.push_back(window.at<uchar>(2, 1) >= threshold);
-			binary_cod.push_back(window.at<uchar>(2, 0) >= threshold);
-			binary_cod.push_back(window.at<uchar>(1, 0) >= threshold);
-			// Uniform LBP value
-			int lbp_value = 0;
-			for (int i = 0; i < binary_cod.size(); i++)
-				lbp_value += binary_cod.at(i) * pow(2, binary_cod.size() - 1 - i);
-			output.at<uchar>(r, c) = toULBP[lbp_value];
-		}
-	}
-	return output;
-}
-
-Mat get_hist2(Mat src,String title,bool show)
-{
-	int histSize = 59;
-	float range[] = { 0, 59 };
-	const float* histRange = { range };
-
-	bool uniform = true; bool accumulate = false;
-	Mat hist;
-
-	calcHist(&src, 1, 0, Mat(), hist, 1, &histSize, &histRange, uniform, accumulate);
-	//cout << "hist... " << hist << endl;
-	// Draw the histograms for B, G and R
-	int hist_w = 512; int hist_h = 400;
-	int bin_w = cvRound((double)hist_w / histSize);
-
-	Mat histImage(hist_h, hist_w, CV_8UC3, Scalar(0, 0, 0));
-	/// Normalize the result to [ 0, histImage.rows ]
-	normalize(hist, hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
-	
-	if (show == true) {
-		for (int i = 1; i < histSize; i++)
-		{
-			line(histImage, Point(bin_w*(i - 1), hist_h - cvRound(hist.at<float>(i - 1))),
-				Point(bin_w*(i), hist_h - cvRound(hist.at<float>(i))),
-				Scalar(256, 0, 0), 2, 8, 0);
-		}
-
-		namedWindow(title, CV_WINDOW_AUTOSIZE);
-		imshow(title, histImage);
-		waitKey(0);
-	}
-
-	return hist;
-}
-
 Mat get_hist(Mat src, String title, bool show)
 {
 	int histSize = 59;
@@ -137,7 +63,7 @@ Mat get_hist(Mat src, String title, bool show)
 	for (int r = 0; r < src.rows; r++) {
 		for (int c = 0; c < src.cols; c++) {
 			int ind = src.at<unsigned char>(r, c);
-			hist.at<float>(0,ind) = hist.at<float>(0,ind) + 1;
+			hist.at<float>(0, ind) = hist.at<float>(0, ind) + 1.0 / (src.rows*src.cols);
 		}
 	}
 
@@ -150,7 +76,7 @@ Mat get_hist(Mat src, String title, bool show)
 
 	Mat histImage(hist_h, hist_w, CV_8UC3, Scalar(0, 0, 0));
 	/// Normalize the result to [ 0, histImage.rows ]
-	normalize(hist, hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
+	//normalize(hist, hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
 
 	if (show == true) {
 		for (int i = 1; i < histSize; i++)
@@ -385,56 +311,56 @@ int main(void)
 	Mat y1_test = Mat::zeros(x_1.rows - x_0.rows*0.7, 1, CV_32SC1);
 	for (int i = 0; i < y1_train.rows; i++) { y1_train.at<int>(i, 0) = 1; }
 	for (int i = 0; i < y1_test.rows; i++) { y1_test.at<int>(i, 0) = 1; }
-	//y1_train.convertTo(y1_train, CV_32SC1);
-	//y1_test.convertTo(y1_test, CV_32SC1);
-	//for (int i=0;)
-	//cout << "train label chino" << endl;
-	//cout << y0_train << endl;
-	//cout << "train label negro" << endl;
-	//cout << y1_train << endl;
-	//cvWaitKey(0);
-	//imshow("asian train", y0_train);
-	//imshow("asian test", y1_test);
-	//cvWaitKey(0);
+	
 	Mat x_train, x_test, y_train, y_test;
 	vconcat(x0_train, x1_train, x_train);
 	vconcat(x0_test, x1_test, x_test);
 	vconcat(y0_train, y1_train, y_train);
 	vconcat(y0_test, y1_test, y_test);
 
-
 	x_train.convertTo(x_train, CV_32FC1);
 	x_test.convertTo(x_test, CV_32FC1);
 
 
-
-
-	// Models
-	Mat y_pred, diff, nonZeroCoordinates;
-	float accuracy, misses;
-
 	// SVM
-	//for (int i = 0, i < 5; i++) {
+	Mat y_pred;
+	//int maxcount[20] = { 10,15,18,20,22,50,100,500,1000,2500,4000,4500,5000,5250,5500,6000,7500,10000,100000,1000000 };
+	int maxcount[5] = { 20,100,5000,10000,100000};
+	//float e[5] = { 0.01,0.001,0.0001,0.00001,0.000001 };
+	double c_value[8] = {0.000001,0.001,1,1000,10000,100000,1000000,10000000 };
+	float e[2] = { 0.001,0.00001};
+	float best_acc = 0.0;
+	int best_m = 10;
+	float best_e = 0.001;
+	double best_C = 1.0;
+	for (int k = 0; k < size(c_value); k++) {
+		for (int j = 0; j < size(e); j++) {
+			for (int i = 0; i < size(maxcount); i++) {
+				Ptr<SVM> svm = SVM::create();
+				svm->setType(SVM::C_SVC);
+				svm->setKernel(SVM::LINEAR);
+				svm->setC(c_value[k]);
+				//svm->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER, maxcount[i], e[j])); // -----------100 0.001
+				svm->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER, maxcount[i], e[j])); // -----------100 0.001
+				svm->train(x_train, ROW_SAMPLE, y_train);
+				//Mat y_pred;
+				svm->predict(x_test, y_pred);
 
-	//}
-	Ptr<SVM> svm = SVM::create();
-	svm->setType(SVM::C_SVC);
-	svm->setKernel(SVM::LINEAR);
-	svm->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER, 18, 0.001)); // -----------100 0.001
-	svm->train(x_train, ROW_SAMPLE, y_train);
-	svm->predict(x_test, y_pred);
-	//cout << "y_test: " << y_test.flags << endl;
-	//cout << y_test << endl;
-	//cout << "y_pred: " << y_pred.flags << endl;
-	//cout << y_pred << endl;
+				float svm_acc = get_acc(y_test, y_pred);
+				cout << "SVM Accuracy: " << svm_acc * 100 << "% C: " << c_value[k] << " param: " << maxcount[i] << " y " << e[j] << endl;
+				if (svm_acc > best_acc) {
+					best_acc = svm_acc;
+					best_m = maxcount[i];
+					best_e = e[j];
+					best_C = c_value[k];
+				}
 
-	//absdiff(y_pred, y_test, diff);
-	//diff.convertTo(diff, CV_8UC1);
-	//findNonZero(diff, nonZeroCoordinates);
-	//misses = nonZeroCoordinates.total();
-	//accuracy = 1 - misses / diff.size().height;
-	float svm_acc = get_acc(y_test, y_pred);
-	cout << "SVM Accuracy: "<< svm_acc * 100 << "%" << endl;
+			}
+		}
+	}
+	cout << "------------------------------" << endl;
+	cout << "Best SVM Accuracy: " << best_acc * 100 << "% C: " << best_C<< " param: " << best_m << " y " << best_e << endl;
+	cout << "------------------------------" << endl;
 
 	// Random Forest
 	Ptr<ml::RTrees> randomForest = ml::RTrees::create();
@@ -444,8 +370,7 @@ int main(void)
 	float rf_acc = get_acc(y_test, y_pred);
 	cout << "Random Forrest Accuracy: " << rf_acc * 100 << "%" << endl;
 
-	cvWaitKey(0);
-	cvWaitKey(0);
+	waitKey(0);
 
 	
 	return 0; // Sale del programa normalmente
